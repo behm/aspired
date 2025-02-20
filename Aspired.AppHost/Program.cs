be613ffef161b739255a015bc7a1fb91079c52af
Brian Behm <bbehm@omnitech-inc.com>
2025-02-20 17:26:32 -0600

    Add SQL Server migrations

--- Aspired.AppHost/Program.cs
+++ Aspired.AppHost/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.Configuration;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
@@ -21,19 +19,25 @@ var sqlServer = builder
     .AddSqlServer("aspired-sql", sqlPassword, 7890)
     .WithDataVolume("aspired-data")
     .WithLifetime(ContainerLifetime.Persistent)
-    .WithBindMount("./SqlServerConfig", target: "/usr/config")                          // NOTE: ensure all script are UTF-8 with LF line endings for Linux container
+//    .WithBindMount("./SqlServerConfig", target: "/usr/config")                          // NOTE: ensure all script are UTF-8 with LF line endings for Linux container
     //.WithBindMount("../Aspired.Database/sql", target: "/docker-entrypoint-initdb.d")    // NOTE: ensure all script are UTF-8 with LF line endings for Linux container
-    .WithBindMount("./Database", target: "/docker-entrypoint-initdb.d")                 // NOTE: ensure all script are UTF-8 with LF line endings for Linux container
-    .WithEntrypoint("/usr/config/entrypoint.sh");
+//    .WithBindMount("./Database", target: "/docker-entrypoint-initdb.d")                 // NOTE: ensure all script are UTF-8 with LF line endings for Linux container
+//    .WithEntrypoint("/usr/config/entrypoint.sh");
+    ;
     //.PublishAsConnectionString()  // if you want to just publish this as a connection string
     //.PublishAsAzureSqlDatabase()  // todo: come back to this
 
 var sqlDatabase = sqlServer.AddDatabase("aspired-db", "Aspired");
 
-
-var apiService = builder.AddProject<Projects.Aspired_ApiService>("apiservice")
+var dbMigrator = builder.AddProject<Projects.Aspired_DatabaseMigrations>("db-migrator")
+    .WithReference(sqlDatabase)
     .WithReference(sqlServer)
     .WaitFor(sqlServer);
+
+var apiService = builder.AddProject<Projects.Aspired_ApiService>("apiservice")
+    .WithReference(sqlDatabase)
+    .WaitFor(sqlDatabase)
+    .WaitForCompletion(dbMigrator);
 
 builder.AddProject<Projects.Aspired_Web>("webfrontend")
     .WithExternalHttpEndpoints()
@@ -41,6 +45,15 @@ builder.AddProject<Projects.Aspired_Web>("webfrontend")
     .WithReference(apiService)
     .WaitFor(cache)
     .WaitFor(apiService);
+
+// NOTE: coming soon???  Check this out!  I think it's pretty much an OData endpoint for your database???
+//       https://aka.ms/dab/aspire
+//var dabConfig = "./dab-config.json";
+//var dabServer = builder
+//    .AddContainer("data-api", "mcr.microsoft.com/azure-databases/data-api-builder")
+//    .WithBindMount(dabConfig, "/App/dab-config.json")
+//    .WithHttpEndpoint(5000, 5000, "http")
+//    .WithReference(sqlServer);
 
 // NOTE: coming soon???  Check this out!  I think it's pretty much an OData endpoint for your database???
 //       https://aka.ms/dab/aspire
---
+++ Aspired.DatabaseMigrations/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DbUp;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Let's migrate some database changes!");
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.AddSqlServerClient("aspired-sql");
+
+var host = builder.Build();
+
+var configuration = host.Services.GetRequiredService<IConfiguration>();
+
+var connectionString = configuration.GetConnectionString("aspired-db");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    Console.WriteLine("No connection string found!");
+}
+
+// ============================================================================
+// Start database migrations
+// ============================================================================
+ 
+EnsureDatabase.For.SqlDatabase(connectionString);
+
+var upgradeEngine = DeployChanges.To
+    .SqlDatabase(connectionString)
+    .LogToConsole() // todo: figure out better logging solution (i.e. ILogger???)
+    .WithScriptsFromFileSystem("Scripts")
+    .Build();
+
+var result = upgradeEngine.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.WriteLine("Upgrade FAILED");    // todo: log this
+    return;
+}
+
+Console.WriteLine("Upgrade was SUCCESSFUL!!!"); // todo: log this
